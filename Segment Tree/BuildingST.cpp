94e1aff5d44018e443549dce82e7dfa1b7ef379e
Raushan kumar <84537309+Rau379@users.noreply.github.com>
2023-02-02 21:34:10 +0530

    Add files via upload

---
+++ Segment Tree/BuildingST.cpp
@@ -0,0 +1,125 @@
+
+#include <iostream>
+using namespace std;
+// COnsttruction of Segment Tree
+int CST(int ss, int se, int si, int arr[], int tree[])
+{
+	if(ss == se)
+	{
+		tree[si] = arr[ss];
+		return arr[ss];
+	}
+
+	int mid = (ss + se) / 2;
+
+	tree[si] = CST(ss, mid, 2 * si + 1, arr, tree)
+		       +CST(mid + 1, se, 2 * si + 2, arr, tree);
+
+	return tree[si];
+}	
+	
+int main() {
+	int arr[] = {10, 20, 30, 40}, n= 4;
+
+    int tree[4 * n];
+    cout << CST(0, n - 1, 0, arr, tree) << endl;
+		
+	return 0;
+}
+
+
+
+// Range Query on Segment Tree
+// getsum(0,2), getsum(1,3), getsum(0,3);
+int CST(int ss, int se, int si, int arr[], int tree[])
+{
+	if(ss == se)
+	{
+		tree[si] = arr[ss];
+		return arr[ss];
+	}
+
+	int mid = (ss + se) / 2;
+
+	tree[si] = CST(ss, mid, 2 * si + 1, arr, tree)
+		       +CST(mid + 1, se, 2 * si + 2, arr, tree);
+
+	return tree[si];
+}
+
+int getSumRec(int qs, int qe, int ss, int se, int si, int tree[])
+{
+	if(se < qs || ss > qe)
+		return 0;
+	if(qs <= ss &&  qe >= se)
+		return tree[si];
+
+	int mid = (ss + se) / 2;
+
+	return getSumRec(qs, qe, ss, mid, 2 * si + 1, tree)
+		   + getSumRec(qs, qe, mid + 1, se, 2 * si + 2, tree);
+}
+	
+int main() {
+	int arr[] = {10, 20, 30, 40}, n= 4;
+
+    int tree[4 * n];
+    CST(0, n - 1, 0, arr, tree);
+     
+    cout << getSumRec(0, 2, 0, 3, 0, tree); 
+		
+	return 0;
+}
+
+
+// Update Query in Segment Tree
+// update 
+
+(1,25), update(0, 15)
+// getsu(0,1), getsum(1,2)
+#include <iostream>
+using namespace std;
+
+int CST(int ss, int se, int si, int arr[], int tree[])
+{
+	if(ss == se)
+	{
+		tree[si] = arr[ss];
+		return arr[ss];
+	}
+
+	int mid = (ss + se) / 2;
+
+	tree[si] = CST(ss, mid, 2 * si + 1, arr, tree)
+		       +CST(mid + 1, se, 2 * si + 2, arr, tree);
+
+	return tree[si];
+}
+
+void updateRec(int ss, int se, int i, int si, int diff, int tree[])
+{
+    if(i < ss || i > se) 
+        return;
+        
+    tree[si] = tree[si] + diff;
+    
+    if(se > ss)
+    {
+        int mid = (ss + se) / 2;
+        
+        updateRec(ss, mid, i, 2 * si + 1, diff, tree);
+        updateRec(mid + 1, se, i, 2 * si + 2, diff, tree);
+        
+    }
+}
+	
+int main() {
+	int arr[] = {10, 20, 30, 40}, n= 4;
+
+    int tree[4 * n];
+    CST(0, n - 1, 0, arr, tree);
+     
+    updateRec(0, 3, 1, 0, 5, tree);
+		
+	return 0;
+}
